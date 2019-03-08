015849926b4f68fbc2b47c738661730a67cc41b7
eksperimental <eksperimental@users.noreply.github.com>
2019-03-08 08:13:02 +0700

    Initial commit

---
+++ lib/string_to_alias.ex
@@ -0,0 +1,18 @@
+defmodule StringToAlias do
+  @moduledoc """
+  Documentation for StringToAlias.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StringToAlias.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/string_to_alias_test.exs
@@ -0,0 +1,8 @@
+defmodule StringToAliasTest do
+  use ExUnit.Case
+  doctest StringToAlias
+
+  test "greets the world" do
+    assert StringToAlias.hello() == :world
+  end
+end
