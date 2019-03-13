82b85dd4ab206496bcb412e3668f552dac4a6729
eksperimental <eksperimental@users.noreply.github.com>
2019-03-14 05:56:47 +0700

    Move validate_alias_string/2 to validate_alias_string/1

--- lib/module/alias_string_to_atom.ex
+++ lib/module/alias_string_to_atom.ex
@@ -32,36 +32,39 @@ defmodule Module.AliasStringToAtom do
   defp valid_alias_string_to_atom(string),
     do: String.to_atom("Elixir." <> string)
 
+  @spec validate_alias_string(String.t()) :: boolean
+  def validate_alias_string(alias_string) do
+    validate_alias_string(alias_string, true)
+  end
 
-  @spec validate_alias_string(String.t(), first_char? :: boolean) :: boolean
-  def validate_alias_string(<<first_char, rest::binary>>, true = _first_char?)
-      when first_char >= ?A and first_char <= ?Z do
+  defp validate_alias_string(<<first_char, rest::binary>>, _first_char? = true)
+       when first_char >= ?A and first_char <= ?Z do
     validate_alias_string(rest, false)
   end
 
-  def validate_alias_string(_, true) do
+  defp validate_alias_string(_, true) do
     false
   end
 
-  def validate_alias_string("", false) do
+  defp validate_alias_string("", false) do
     true
   end
 
-  def validate_alias_string(".", false) do
+  defp validate_alias_string(".", false) do
     false
   end
 
-  def validate_alias_string(<<".", rest::binary>>, false) do
+  defp validate_alias_string(<<".", rest::binary>>, false) do
     validate_alias_string(rest, true)
   end
 
-  def validate_alias_string(<<char, rest::binary>>, false)
-      when (char >= ?A and char <= ?Z) or (char >= ?a and char <= ?z) or
-             (char >= ?0 and char <= ?9) or char == ?_ do
+  defp validate_alias_string(<<char, rest::binary>>, false)
+       when (char >= ?A and char <= ?Z) or (char >= ?a and char <= ?z) or
+              (char >= ?0 and char <= ?9) or char == ?_ do
     validate_alias_string(rest, false)
   end
 
-  def validate_alias_string(_, false) do
+  defp validate_alias_string(_, false) do
     false
   end
 end
